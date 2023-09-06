3517580c6a8f51a00e161a3ba0c04c9f25ce78da
osnaider <osnaidermartinez2002@gmail.com>
2023-09-05 22:11:33 -0500

    card reusable

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,23 +1,25 @@
-
 import { Link } from "react-router-dom";
 import { FaPlay } from "react-icons/fa";
-import './card.css'
-export const Card = () => {
+import "./card.css";
+export const Card = ({ image, title, description }) => {
   return (
     <>
-      <Link to="/" className="w-[180px]">
-        <div className="w-full bg-[rgba(255,255,255,0.3)] rounded-md overflow-hidden p-4 content-card">
+      <Link to="/" className="max-w-[180px]">
+        <div className="w-full bg-[#151515] hover:bg-[rgba(255,255,255,0.1)] rounded-md overflow-hidden p-4 transition-colors duration-200 content-card">
           <div className="relative w-[150px] h-[150px] bg-red-500 rounded-md overflow-hidden">
+            <img
+              src={image}
+              alt={title}
+              className="w-full h-full object-cover"
+            />
             <button className="absolute right-3 opacity-0 w-[45px] h-[45px] bg-green-500 rounded-full -bottom-16 z-10 transition-all duration-200">
               <i className="w-full h-full flex justify-center items-center text-black">
                 <FaPlay />
               </i>
             </button>
           </div>
-          <strong className="text-sm">Release Radar</strong>
-          <p className="uppercase text-xs line-clamp-2">
-            grupo niche , Gilberto santa rosa , dimension
-          </p>
+          <strong className="text-sm line-clamp-1">{title}</strong>
+          <p className="capitalize text-xs line-clamp-2">{description}</p>
         </div>
       </Link>
     </>
