c344081e6f80f2f3c7c08987af816d37393d500c
osnaider <osnaidermartinez2002@gmail.com>
2023-09-07 21:06:26 -0500

    add loader card

---
+++ src/components/loader/LoaderCard.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import "./loaderCard.css";
+export const LoaderCard = () => {
+  return (
+    <div className="relative flex flex-wrap justify-start gap-4 items-center">
+      <div className=" h-[226px] p-4 w-[180px] rounded-md overflow-hidden loader-animation">
+        <div className="relative w-[150px] h-[150px] rounded-md  loader-img"></div>
+        <div className="relative w-[90%] min-h-4 h-4 mt-2 rounded-md loader-img"></div>
+        <div className="relative w-[60%] min-h-4 h-4 mt-1 rounded-md loader-img "></div>
+      </div>
+      <div className=" h-[226px] p-4 w-[180px] rounded-md overflow-hidden loader-animation">
+        <div className="relative w-[150px] h-[150px] rounded-md  loader-img"></div>
+        <div className="relative w-[90%] min-h-4 h-4 mt-2 rounded-md loader-img"></div>
+        <div className="relative w-[60%] min-h-4 h-4 mt-1 rounded-md loader-img "></div>
+      </div>
+      <div className=" h-[226px] p-4 w-[180px] rounded-md overflow-hidden loader-animation">
+        <div className="relative w-[150px] h-[150px] rounded-md  loader-img"></div>
+        <div className="relative w-[90%] min-h-4 h-4 mt-2 rounded-md loader-img"></div>
+        <div className="relative w-[60%] min-h-4 h-4 mt-1 rounded-md loader-img "></div>
+      </div>
+      <div className=" h-[226px] p-4 w-[180px] rounded-md overflow-hidden loader-animation">
+        <div className="relative w-[150px] h-[150px] rounded-md  loader-img"></div>
+        <div className="relative w-[90%] min-h-4 h-4 mt-2 rounded-md loader-img"></div>
+        <div className="relative w-[60%] min-h-4 h-4 mt-1 rounded-md loader-img "></div>
+      </div>
+      <div className=" h-[226px] p-4 w-[180px] rounded-md overflow-hidden loader-animation">
+        <div className="relative w-[150px] h-[150px] rounded-md  loader-img"></div>
+        <div className="relative w-[90%] min-h-4 h-4 mt-2 rounded-md loader-img"></div>
+        <div className="relative w-[60%] min-h-4 h-4 mt-1 rounded-md loader-img "></div>
+      </div>
+      <div className=" h-[226px] p-4 w-[180px] rounded-md overflow-hidden loader-animation">
+        <div className="relative w-[150px] h-[150px] rounded-md  loader-img"></div>
+        <div className="relative w-[90%] min-h-4 h-4 mt-2 rounded-md loader-img"></div>
+        <div className="relative w-[60%] min-h-4 h-4 mt-1 rounded-md loader-img "></div>
+      </div>
+      <div className=" h-[226px] p-4 w-[180px] rounded-md overflow-hidden loader-animation">
+        <div className="relative w-[150px] h-[150px] rounded-md  loader-img"></div>
+        <div className="relative w-[90%] min-h-4 h-4 mt-2 rounded-md loader-img"></div>
+        <div className="relative w-[60%] min-h-4 h-4 mt-1 rounded-md loader-img "></div>
+      </div>
+      <div className=" h-[226px] p-4 w-[180px] rounded-md overflow-hidden loader-animation">
+        <div className="relative w-[150px] h-[150px] rounded-md  loader-img"></div>
+        <div className="relative w-[90%] min-h-4 h-4 mt-2 rounded-md loader-img"></div>
+        <div className="relative w-[60%] min-h-4 h-4 mt-1 rounded-md loader-img "></div>
+      </div>
+      <div className=" h-[226px] p-4 w-[180px] rounded-md overflow-hidden loader-animation">
+        <div className="relative w-[150px] h-[150px] rounded-md  loader-img"></div>
+        <div className="relative w-[90%] min-h-4 h-4 mt-2 rounded-md loader-img"></div>
+        <div className="relative w-[60%] min-h-4 h-4 mt-1 rounded-md loader-img "></div>
+      </div>
+      <div className=" h-[226px] p-4 w-[180px] rounded-md overflow-hidden loader-animation">
+        <div className="relative w-[150px] h-[150px] rounded-md  loader-img"></div>
+        <div className="relative w-[90%] min-h-4 h-4 mt-2 rounded-md loader-img"></div>
+        <div className="relative w-[60%] min-h-4 h-4 mt-1 rounded-md loader-img "></div>
+      </div>
+    </div>
+  );
+};
