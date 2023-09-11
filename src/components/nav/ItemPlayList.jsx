e2664459d52999d6bb3678905536b89c5eaea122
osnaider <osnaidermartinez2002@gmail.com>
2023-09-10 21:39:43 -0500

    modicfic style

--- src/components/nav/ItemPlayList.jsx
+++ src/components/nav/ItemPlayList.jsx
@@ -1,31 +1,43 @@
 import { FiMusic } from "react-icons/fi";
 import { useSelector } from "react-redux";
 import { useGetPlayListsUserQuery } from "../../redux/facture/stateHome/playLists";
-
+import { Link } from "react-router-dom";
+import { TbPointFilled } from "react-icons/tb";
 export const ItemPlayList = () => {
-
   const { responseToken } = useSelector((state) => state.authUser);
-  const { data } = useGetPlayListsUserQuery(responseToken.access_token);
-  
+  const { data } = useGetPlayListsUserQuery(responseToken?.access_token);
+  console.log(data);
   return (
-    <div className="px-4 mt-3">
+    <div className="px-4 mt-3 ">
       {data?.items?.map((lists) => (
-        <div
-          className="flex gap-x-3 px-2 py-2 hover:bg-[#1A1A1A] rounded-lg cursor-pointer transition-colors duration-150"
-          key={lists.id}
-        >
-          <div className="h-[50px]  w-[50px] bg-[#282828] rounded-lg text-[#B3B3B3]">
-            <i className="flex justify-center items-center w-full h-full text-3xl">
-              <FiMusic />
-            </i>
+        <Link key={lists.id} to={`/playlist/${lists?.id}`}>
+          <div className="flex gap-x-3  px-2 py-2 hover:bg-[rgba(255,255,255,0.1)] rounded-lg cursor-pointer transition-colors duration-150">
+            <div className="h-[50px]  w-[50px] bg-[#282828] rounded-lg text-[#B3B3B3] overflow-hidden">
+              {lists?.images[2]?.url ? (
+                <img
+                  src={lists?.images[2]?.url}
+                  alt=""
+                  className="w-full h-full"
+                />
+              ) : (
+                <i
+                  className="flex justify-center items-center w-full 
+                h-full  text-3xl"
+                >
+                  <FiMusic />
+                </i>
+              )}
+            </div>
+            <div className="flex flex-col justify-between gap-x-3">
+              <span className="block ">{lists?.name}</span>
+              <div className="text-[15px] capitalize flex justify-center gap-x-1 items-center">
+                <span>{lists.type} </span>
+                <TbPointFilled className="flex text-[10px]"/> 
+                <span>{lists?.owner?.display_name}</span>
+              </div>
+            </div>
           </div>
-          <div className="flex flex-col gap-x-3">
-            <span className="block ">{lists?.name}</span>
-            <strong className="text-sm">
-              {lists.type} {lists?.display_name}
-            </strong>
-          </div>
-        </div>
+        </Link>
       ))}
     </div>
   );
