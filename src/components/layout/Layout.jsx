7643ce9d30a126601f31b1579efc8de080b97df0
osnaider <osnaidermartinez2002@gmail.com>
2023-09-14 21:32:31 -0500

    scroll no visible

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,18 +1,16 @@
 import { Header } from "../header/Header";
 import { Nav } from "../nav/Nav";
 import { Footer } from "../footer/Footer";
-import './layout.css'
+import "./layout.css";
 export function Layout({ children }) {
   return (
     <>
-      <div className="p-2 relative  flex h-screen bg-black w-full">
+      <div className="p-2 relative font-lato flex h-screen bg-black w-full">
         <Nav />
         <div className="relative w-full h-full mx-2 rounded-lg overflow-hidden overflow-y-auto">
           <Header />
-          <main className="relative h-[86vh]  overflow-hidden overflow-y-auto rounded-lg bg-[#121212]">
-            <div className="relative h-full w-full overflow-hidden overflow-y-auto rounded-lg layout">
-              {children}
-            </div>
+          <main className="relative h-[86vh] overflow-hidden rounded-lg  layout">
+            {children}
           </main>
         </div>
         <Footer />
