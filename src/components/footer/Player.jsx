d0f31b9bdd8c38850d94ad2f1bf3cf7239cef510
osnaider <osnaidermartinez2002@gmail.com>
2023-09-16 21:54:33 -0500

    add play tracks

--- src/components/footer/Player.jsx
+++ src/components/footer/Player.jsx
@@ -1,23 +1,52 @@
-import { BsShuffle , BsRepeat} from "react-icons/bs";
+import { BsShuffle, BsRepeat } from "react-icons/bs";
 import { BiSkipPrevious, BiSkipNext } from "react-icons/bi";
 import { ButtonRunSong } from "./ButtonRunSong";
 import { ButtonPlay } from "./ButtonPlay";
+import { useSelector } from "react-redux";
+import { useRef, useState } from "react";
+import { transformDuration } from "../../helpers/transform";
+
 export const Player = () => {
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0);
+  const { track } = useSelector((state) => state.players);
+  const audioRef = useRef(null);
+  const handleTimeUpdate = () => {
+    const audioElement = audioRef.current;
+    if (audioElement) {
+      setCurrentTime(audioElement.currentTime);
+      setDuration(audioElement.duration);
+    }
+  };
   return (
     <div>
       <div>
         <div className="flex justify-center items-center gap-x-3 py-1">
-          <ButtonRunSong icon={<BsShuffle className="text-xl"/>}/>
+          <ButtonRunSong icon={<BsShuffle className="text-xl" />} />
           <ButtonRunSong icon={<BiSkipPrevious className="text-4xl" />} />
-          <ButtonPlay/>
-          <ButtonRunSong icon={<BiSkipNext className="text-4xl"/>} />
-          <ButtonRunSong icon={<BsRepeat className="text-xl"/>} />
+          <div>
+            <ButtonPlay />
+            <audio
+              src={track?.preview_url}
+              autoPlay
+              ref={audioRef}
+              type="audio"
+              onTimeUpdate={handleTimeUpdate}
+            ></audio>
+             
+          </div>
+          <ButtonRunSong icon={<BiSkipNext className="text-4xl" />} />
+          <ButtonRunSong icon={<BsRepeat className="text-xl" />} />
         </div>
         <div>
           <div className="flex justify-center items-center w-[500px] text-[11px]">
-            <label htmlFor="" className="m-1">00</label>
-            <input type="range" className="range-vol"/>
-            <label htmlFor="" className="m-1">300</label>
+            <label htmlFor="" className="m-1">
+              {transformDuration(currentTime * 1000)}
+            </label>
+            <input type="range" value={currentTime} max={duration} className="min-w-[300px] max-w-[300px] w-full"></input>
+            <label htmlFor="" className="m-1">
+              {transformDuration(duration * 1000)}
+            </label>
           </div>
         </div>
       </div>
--- src/components/footer/SongRun.jsx
+++ src/components/footer/SongRun.jsx
@@ -1,19 +1,59 @@
-import {CiHeart} from "react-icons/ci"
-import {CgInpicture} from 'react-icons/cg'
+import { CiHeart } from "react-icons/ci";
+import { CgInpicture } from "react-icons/cg";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { toast } from "sonner";
 export const SongRun = () => {
+  const { track } = useSelector((state) => state.players);
+  console.log(track);
   return (
-    <div className="flex flex-grow">
-      <div className="flex px-2 py-1 gap-x-5 justify-center items-center">
-        <div className="h-[55px] w-[55px] bg-blue-500 rounded-lg overflow-hidden"></div>
-        <div>
-          <p>cancion</p>
-          <span className="text-[12px]">generos , generos</span>
-        </div>
-        <div className="flex gap-x-2 py-1 ml-2">
-          <i className="text-2xl text-[#ffffff80] cursor-pointer hover:scale-105 transition-all duration-200 hover:text-white"><CiHeart/></i>
-          <i className="text-2xl text-[#ffffff80] cursor-pointer hover:scale-105 transition-all duration-200 hover:text-white"><CgInpicture/></i>
+    <>
+      <div className="flex flex-grow  min-w-[390px]">
+        <div className="flex px-2 py-1 gap-x-5 justify-center items-center">
+          <div className="h-[55px] w-[55px]  rounded-lg overflow-hidden">
+            <img
+              src={track?.album?.images[2].url}
+              alt=""
+              className="w-full h-full object-cover"
+            />
+          </div>
+          <div>
+            <Link to={`track/${track?.id}`} className="text-sm hover:underline">
+              {track?.name}
+            </Link>
+            <div className="text-[12px] flex justify-start items-center">
+              {track?.artists?.map((artist) => (
+                <Link
+                  key={artist.id}
+                  to={`artist/${artist.id}`}
+                  className="hover:underline mr-2"
+                >
+                  {artist.name}
+                </Link>
+              ))}
+            </div>
+          </div>
+          <div className="flex gap-x-2 py-1 ml-2">
+            <i className="text-2xl text-[#ffffff80] cursor-pointer hover:scale-105 transition-all duration-200 hover:text-white">
+              <CiHeart />
+            </i>
+            <i className="text-2xl text-[#ffffff80] cursor-pointer hover:scale-105 transition-all duration-200 hover:text-white ">
+              <CgInpicture />
+            </i>
+          </div>
         </div>
       </div>
-    </div>
+      {track?.preview_url === null &&
+        toast.error("info" , {
+          description: `la track ${track?.name} no tiene preview`,
+          style : {
+            background : "#121212",
+            color : "#1FDF64",
+            borderColor : "#1FDF64",
+            fontSize : "14px",
+            textTransform : "capitalize"
+          },
+        })}
+    </>
   );
 };
