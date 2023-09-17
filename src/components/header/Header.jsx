2c20d46caa6f2492125a408af4d01b0469d78b79
osnaider <osnaidermartinez2002@gmail.com>
2023-09-16 21:59:40 -0500

    configure position

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,7 @@ import { HeaderNav } from "./HeaderNav";
 
 export const Header = () => {
   return (
-    <div className="absolute top-0 left-0  w-full h-[65px] bg-[rgba(255,255,255,0.1) z-30">
+    <div className="absolute top-0  left-0  w-full h-[65px] bg-[rgba(255,255,255,0.1) z-30 ">
       <div className="flex justify-between px-4 items-center">
         <ButtonNavegation />
         <HeaderNav/>
