bd5fce71589e6ee037f7eedfe0542f9e92f9465f
osnaider <osnaidermartinez2002@gmail.com>
2023-09-17 17:45:55 -0500

    add loader playlist

---
+++ src/components/loader/LoaderPlayList.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./loaderPlayList.css";
+export const LoaderPlayList = () => {
+  return (
+    <div className="h-screen relative w-full flex justify-center items-center">
+      <span class="loader"></span>
+    </div>
+  );
+};
