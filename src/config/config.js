3e176c925ec70de5c33af0231ff270f2f2ebecb4
osnaider <osnaidermartinez2002@gmail.com>
2023-09-18 09:33:26 -0500

    configure variables

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
 export const CLIENT_ID = import.meta.env.VITE_CLIENT_ID
 export const CLIENT_SECRET = import.meta.env.VITE_CLIENT_SECRET
-export const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI_PRODUCCION
+export const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI
