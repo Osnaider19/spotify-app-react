e97e4be7452f8c0e4551f46be32a5feda2a33514
osnaider <osnaidermartinez2002@gmail.com>
2023-09-19 11:09:18 -0500

    add funtion back , forward

--- src/components/header/ButtonNavegation.jsx
+++ src/components/header/ButtonNavegation.jsx
@@ -1,13 +1,26 @@
-import { IoIosArrowForward  , IoIosArrowBack} from "react-icons/io";
+import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
+
 export const ButtonNavegation = () => {
+  const navigate = useNavigate();
+  const handelGoBack = () => {
+    navigate(-1);
+  };
+  const handelGoForward = () => {
+    navigate(+1);
+  };
+ 
   return (
     <div className="py-3  flex gap-x-2">
-      <button>
+      <button
+        onClick={handelGoBack}
+        
+      >
         <i className="h-[35px] cursor-pointer w-[35px] text-[#ffffff80] hover:text-[#fff]  rounded-full flex flex-col justify-center items-center text-2xl bg-[rgba(255,255,255,0.1)]">
           <IoIosArrowBack />
         </i>
       </button>
-      <button>
+      <button onClick={handelGoForward}>
         <i className="h-[35px] cursor-pointer w-[35px] text-[#ffffff80] hover:text-[#fff]  rounded-full flex flex-col justify-center items-center text-2xl bg-[rgba(255,255,255,0.1)]">
           <IoIosArrowForward />
         </i>
