b7d8fb68d8b647774cee7261d4129bdac15674b4
osnaider <osnaidermartinez2002@gmail.com>
2023-09-20 17:49:48 -0500

    add playlist order

---
+++ src/components/playlist/PlayListsHome.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Card } from "../card/Card";
+
+export const PlayListsHome = ({ playlists }) => {
+  return (
+    <div>
+      <div className="flex flex-wrap justify-start gap-4">
+        {playlists?.map((items) => (
+          <div key={items.id} className="max-w-[180px]">
+            <Card
+              image={items?.images[0]?.url}
+              description={items?.type}
+              title={items?.name}
+              id={items?.id}
+              mediaType={items?.type}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
