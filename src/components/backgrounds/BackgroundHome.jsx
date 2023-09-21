85f3a0124bfa71b803c2807d9e6aefb7a17c5bc9
osnaider <osnaidermartinez2002@gmail.com>
2023-09-20 20:44:13 -0500

    add backgroundhome

---
+++ src/components/backgrounds/BackgroundHome.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const BackgroundHome = () => {
+  return (
+    <div className="absolute left-0 top-0 w-full h-[300px] bg-gradient-to-t from-transparent via-[#1FDF6450] to-[#1FDF6470]"></div>
+  );
+};
