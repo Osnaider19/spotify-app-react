613733ebb072b6b954c62f42df240631060f3275
osnaider <osnaidermartinez2002@gmail.com>
2023-09-20 20:45:36 -0500

    add card vertical in home

---
+++ src/components/card/CardVertical.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Cardvertical.css";
+import { FaPlay } from "react-icons/fa";
+import { Link } from "react-router-dom";
+export const CardVertical = ({ imagen, name , link }) => {
+  return (
+    <Link to={link}>
+      <div className="content__card__vertical">
+        <div className="content__card__vertical__img">
+          <img src={imagen} alt={name} />
+        </div>
+        <div className="content__card__vertical__name">
+          <span>{name}</span>
+        </div>
+        <button>
+          <i className="w-full h-full flex justify-center items-center text-black">
+            <FaPlay />
+          </i>
+        </button>
+      </div>
+    </Link>
+  );
+};
