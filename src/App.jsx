cbe7bc73f67cea8fae6468ce07a7e612ffa14fc0
osnaider <osnaidermartinez2002@gmail.com>
2023-10-06 10:09:30 -0500

    add tanStack Query

--- src/App.jsx
+++ src/App.jsx
@@ -4,91 +4,81 @@ import { HomePage } from "./page/HomePage";
 import { Layout } from "./components/layout/Layout";
 import { Navigate } from "react-router-dom";
 import { Toaster } from "sonner";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { LoginPage } from "./page/LoginPage";
 import { AlbumPage } from "./page/AlbumPage";
 import { ArtistsPage } from "./page/ArtistsPage";
 import { PlayListsPage } from "./page/PlayListsPage";
 import { TrackPage } from "./page/TrackPage";
 import { NotFound } from "./page/NotFound";
-import { useRefreshToken } from "./hooks/useRefreshToken";
+import { useEffect } from "react";
+import { setResponseToken } from "./redux/facture/auth/authSlice";
+import { SpotfifyAuth } from "./helpers/auth";
+
 function App() {
-  const { isAuthenticated } = useSelector(
+  const { isAuthenticated, refresh_token } = useSelector(
     (state) => state.authUser
   );
 
-  useRefreshToken();
-  
+  const dispatch = useDispatch();
+  const useRefreshToken = async () => {
+    let res;
+    try {
+      if (refresh_token) {
+        res = await SpotfifyAuth({
+          grant_type: "refresh_token",
+          refresh_token: refresh_token,
+        });
+      }
+      if (!res.access_token) {
+        throw new Error("error access token es null");
+      }
+      dispatch(setResponseToken(res));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    useRefreshToken();
+  }, []);
+
   return (
     <>
       <Toaster closeButton visibleToasts={4} duration={4000} />
-      <Routes>
-        <Route
-          path="/login"
-          element={isAuthenticated ? <Navigate to="/" /> : <LoginPage />}
-        />
-        <Route path="*" element={<NotFound />} />
-        <Route
-          path="/"
-          element={
-            isAuthenticated ? (
-              <Layout>
-                <HomePage />
-              </Layout>
-            ) : (
-              <Navigate to="/login" />
-            )
-          }
-        />
-        <Route
-          path="/playlist/:id"
-          element={
-            isAuthenticated ? (
-              <Layout>
-                <PlayListsPage />
-              </Layout>
-            ) : (
-              <Navigate to="/login" />
-            )
-          }
-        />
-        <Route
-          path="/album/:id"
-          element={
-            isAuthenticated ? (
-              <Layout>
-                <AlbumPage />
-              </Layout>
-            ) : (
-              <Navigate to="/login" />
-            )
-          }
-        />
-        <Route
-          path="/artist/:id"
-          element={
-            isAuthenticated ? (
-              <Layout>
-                <ArtistsPage />
-              </Layout>
-            ) : (
-              <Navigate to="/login" />
-            )
-          }
-        />
-        <Route
-          path="/track/:id"
-          element={
-            isAuthenticated ? (
-              <Layout>
-                <TrackPage />
-              </Layout>
-            ) : (
-              <Navigate to="/login" />
-            )
-          }
-        />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route
+            path="/login"
+            element={isAuthenticated ? <Navigate to="/" /> : <LoginPage />}
+          />
+          <Route
+            path="/"
+            element={isAuthenticated ? <HomePage /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/playlist/:id"
+            element={
+              isAuthenticated ? <PlayListsPage /> : <Navigate to="/login" />
+            }
+          />
+          <Route
+            path="/album/:id"
+            element={isAuthenticated ? <AlbumPage /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/artist/:id"
+            element={
+              isAuthenticated ? <ArtistsPage /> : <Navigate to="/login" />
+            }
+          />
+          <Route
+            path="/track/:id"
+            element={isAuthenticated ? <TrackPage /> : <Navigate to="/login" />}
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Layout>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,14 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
       <Provider store={store}>
         <App />
       </Provider>
+    </QueryClientProvider>
   </BrowserRouter>
 );
