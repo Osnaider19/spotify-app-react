5dde117f97fe0828c69a9377df22edb65d2c2d56
osnaider <osnaidermartinez2002@gmail.com>
2023-10-09 18:51:13 -0500

    add playing loader

---
+++ src/components/playing/Playing.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './playing.css'
+export const Playing = () => {
+  return (
+    <div class="playing">
+      <div class="greenline line-1"></div>
+      <div class="greenline line-2"></div>
+      <div class="greenline line-3"></div>
+      <div class="greenline line-4"></div>
+      <div class="greenline line-5"></div>
+    </div>
+  );
+};
